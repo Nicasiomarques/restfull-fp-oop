b3cb2c63d778250d523ca9f15b9a13cd8ff38998
Nicasiomarques <nicasiomarques18@gmail.com>
2021-07-15 18:01:12 +0100

    move .env import to server file

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,10 @@
+import * as dotenv from 'dotenv'
+
 import App from "./app";
 import validateEnv from "./utils/valitateEnv";
 import PostController from "./features/post/post.controller";
 
+dotenv.config()
 validateEnv()
 
 const app = new App([new PostController()])
