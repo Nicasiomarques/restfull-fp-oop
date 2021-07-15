b927634a5605d7fa21287e089269def4f445e7fd
Nicasiomarques <nicasiomarques18@gmail.com>
2021-07-15 18:03:40 +0100

    add post exception not found

--- src/http/exceptions/PostNotFoundException.ts
+++ src/http/exceptions/PostNotFoundException.ts
@@ -1,7 +1,6 @@
 import HttpException from "./httpException";
 
 export default class PostNotFoundException extends HttpException {
-  message: string
   constructor(id: string) {
     super(404, `Post not found with id ${id} not found`)
   }
