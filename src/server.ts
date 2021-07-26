2efa753f388506d41b627389b062f4d612c98d66
Nicasiomarques <nicasiomarques18@gmail.com>
2021-07-26 01:42:08 +0100

    add authentication as feature

--- src/server.ts
+++ src/server.ts
@@ -3,10 +3,14 @@ import * as dotenv from 'dotenv'
 import App from "./app";
 import validateEnv from "./utils/valitateEnv";
 import PostController from "./features/post/post.controller";
+import AuthenticationController from './features/authentication/authentication.controller';
 
 dotenv.config()
 validateEnv()
 
-const app = new App([new PostController()])
+const app = new App([
+  new PostController(),
+  new AuthenticationController()
+])
 
 app.run();
