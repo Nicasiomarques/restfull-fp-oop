66fe78a3bf2c628db17a8ace19f0fc2b36e555f8
Nicasiomarques <nicasiomarques18@gmail.com>
2021-08-03 10:42:42 +0100

    add address model

---
+++ src/modules/user/user.model.ts
@@ -0,0 +1,16 @@
+import * as mongoose from 'mongoose';
+import { User } from './user.type';
+
+const addressSchema = new mongoose.Schema({
+  street: String,
+  city: String
+})
+
+const userSchema = new mongoose.Schema({
+  name: String,
+  email: String,
+  password: String,
+  address: addressSchema
+});
+
+export const UserModel = mongoose.model<User & mongoose.Document>('User', userSchema);
